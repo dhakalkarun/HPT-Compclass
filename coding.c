ffcb9d5e718d1fd865390aa320700982af489948
Dhakal Karun <65776419+dhakalkarun@users.noreply.github.com>
2021-09-23 14:06:38 +0545

    First iteration of Assignment of HPT
    I have commented all the codes.

---
+++ coding.c
@@ -0,0 +1,159 @@
+
+// a. Area of rectangle
+
+// #include <stdio.h>
+
+// int main (){
+//     int l, b;
+//     scanf ("%d %d", &l, &b);
+//     int area;
+//     area = l*b;
+    
+//     printf ("Area\t:%d", area);
+//     return 0 ;
+// }
+
+
+// b. Perimeter
+
+// int main (){
+//     int l, b;
+//     scanf ("%d %d", &l, &b);
+//     int perimeter;
+//     perimeter = 2*(l+b);
+    
+//     printf ("Perimeter\t:%d", perimeter);
+//     return 0 ;
+// }
+
+// c. Area circle
+// #include <stdio.h>
+
+
+// int main (){
+//     int r;
+//     int pi = 3.1415;
+//     scanf ("%d", &r);
+//     int area;
+//     area = pi*r*r;
+    
+//     printf ("Area\t:%d", area);
+//     return 0 ;
+// }
+
+// d. Perimeter circle
+
+// #include <stdio.h>
+
+
+// int main (){
+//     int r;
+    
+//     scanf ("%d", &r);
+//     int perimeter;
+//     perimeter = 2*pi*r;
+    
+//     printf ("perimeter\t:%d", perimeter);
+//     return 0 ;
+// }
+
+// e. Sum
+
+// #include <stdio.h>
+
+
+// int main (){
+//     int a, b;
+    
+//     scanf ("%d %d", &a, &b);
+//     int sum;
+//     sum = a + b;
+    
+//     printf ("Sum\t:%d", sum);
+//     return 0 ;
+// }
+
+// f. product
+
+
+// #include <stdio.h>
+
+
+// int main (){
+//     int a, b;
+    
+//     scanf ("%d %d", &a, &b);
+//     int prod;
+//     prod = a * b;
+    
+//     printf ("Product\t:%d", prod);
+//     return 0 ;
+// }
+
+// g. difference
+
+
+// #include <stdio.h>
+
+
+// int main (){
+//     float a, b;
+    
+//     scanf ("%f %f", &a, &b);
+//     float diff;
+//     diff = a - b;
+    
+//     printf ("Difference\t:%f", diff);
+//     return 0 ;
+// }
+
+// h. interest
+
+
+// #include <stdio.h>
+
+
+// int main (){
+//     float p, t, r;
+    
+//     scanf ("%f %f %f", &p, &t, %r);
+//     float interest;
+//     interest = (p*t*r)/100;
+    
+//     printf ("interest\t:%f", interest);
+//     return 0 ;
+// }
+
+// i. dist. travelled
+
+
+// #include <stdio.h>
+
+
+// int main (){
+//     float u, t, a;
+    
+//     scanf ("%f %f %f", &u, &t, &a);
+//     float dist;
+//     dist = u*t+(1/2)*a*t*t;
+    
+//     printf ("distance\t:%f", dist);
+//     return 0 ;
+// }
+
+// profit
+
+
+#include <stdio.h>
+
+
+int main (){
+    float cp, sp;
+    
+    scanf ("%d %d", &cp, &sp);
+    int profit ;
+    profit = sp - cp;
+    
+    printf ("Profit\t:%d", profit);
+    return 0 ;
+}
